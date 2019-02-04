b3ec18c8ef963647d3677a1376ad05b02864c29d
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-02-04 19:17:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,30 +7,23 @@ def roll_call_dwarves(dwarves)
 
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
   def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|calls| calls.capitalize + "!" }
+  planeteer_calls.collect {|calls| calls.capitalize + "!" }
+  #with collect it returns a new array with the operations performed on each one.
+  #curly braces are shorter syntax for do and end, fits on one line.
   end
 
 short_words = ["puff", "go", "two"]
 assorted_words = ["two", "go", "industrious", "bop"]
 
-  def long_planeteer_calls(assorted_words)
-    long_words = []
-    assorted_words.each do |word|
-      if word.length > 4
-      long_words << word
-      end
-    end
-    if long_words.length > 0
-      return true
-    else
-      return false
-    end
-  end
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
+end
 
 
 snacks_array = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese(snacks_array)
   cheese = ["cheddar", "gouda", "camembert"]
-  snacks_array.find { |snack| cheese.include?(snack) }
+  snacks_array.find { |snack| cheese.include?(snack) } #read this backwards - does the snack include cheese?
 end
